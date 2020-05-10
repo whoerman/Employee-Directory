2b2f03ea1f9e744e83a28ee06621c98d7a917e9a
Walter Hoerman <whoerman@gmail.com>
2020-05-10 10:18:13 -0400

    final sorting

--- employeedirectory/src/components/employees/Employee.js
+++ employeedirectory/src/components/employees/Employee.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import Employeecard from './Employeecard';
-//import Search from './Search';
 
 class Employees extends Component {
     render() {
         return (
             <div>
                 {this.props.employees.filter( (employee) => {
-                // if (JSON.stringify(employee.name).contains(Search.userInput)) {
-                //     return true
-                // }
-                //     return false
-                return true
+                if (JSON.stringify(employee).includes(this.props.search)) {
+                    return true
+                }
+                    return false
+                // return true
                 }).map(employee => (
                     <Employeecard key={employee.id} employee={employee} />
                 ))}
